2e9c81aa786e1412bb2a3e9d97fff775cc65bf5c
morgan <mbayer.work@gmail.com>
2019-01-07 15:22:43 -0800

    Updated with correct .java file

--- src/subfactorials/Subfactorials.java
+++ src/subfactorials/Subfactorials.java
@@ -5,6 +5,10 @@
  */
 package subfactorials;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  *
  * @author morgan
@@ -14,8 +18,31 @@ public class Subfactorials {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // TODO code application logic here
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Subfactorial n: ");
+        long input = Integer.parseInt(br.readLine());
+        
+        findSubfactorial(input);
+        
+        System.out.println(findSubfactorial(input));
     }
-    
+
+    private static long findSubfactorial(long input) {
+        long sF = input;
+        
+        if (sF == 0)
+        {
+            return 1;
+        }
+        if (sF == 1)
+        {
+            return 0;
+        }
+        else{
+            return (long)(sF-1)*(findSubfactorial(sF-1)+findSubfactorial(sF-2));
+        }
+        
+    }
 }
